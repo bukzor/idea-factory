0e2ffd7582547bbe3c77a732771d87331b97ce58
Buck Golemon <workitharder@gmail.com>
2012-08-12 20:02:56 -0700

    first try with heroku

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+import os
+
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
@@ -5,10 +8,14 @@ from pyramid.response import Response
 def hello_world(request):
    return Response('Hello %(name)s!' % request.matchdict)
 
-if __name__ == '__main__':
+def main():
    config = Configurator()
    config.add_route('hello', '/hello/{name}')
    config.add_view(hello_world, route_name='hello')
    app = config.make_wsgi_app()
-   server = make_server('0.0.0.0', 8082, app)
+   port = int(os.environ.get("PORT", "8082"))
+   server = make_server('0.0.0.0', port, app)
    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
