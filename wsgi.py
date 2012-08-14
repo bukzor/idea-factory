a01a0f6f50af717ed9099c9ad5ccb9fa4e2d9a67
Buck Golemon <workitharder@gmail.com>
2012-08-14 09:10:18 -0700

    dotcloud compatibility. part 1.

--- wsgi.py
+++ wsgi.py
@@ -7,18 +7,27 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 
 def hello_world(request):
-   return Response('Hello %(name)s!' % request.matchdict)
+    environ = request.environ
+    lines = ['Hello %(name)s!' % request.matchdict, '']
+    lines.extend(
+            '{0}={1}\n'.format(k,environ[k])
+            for k in sorted(environ)
+    )
+    return Response('<br>'.join(lines))
 
-def main():
+def make_app():
     config = Configurator()
     config.add_route('hello', '/hello/{name}')
     config.add_view(hello_world, route_name='hello')
-    app = config.make_wsgi_app()
+    return config.make_wsgi_app()
+
+def run_app(app):
     port = int(os.environ.get("PORT", "8082"))
     server = make_server('0.0.0.0', port, app)
     print >>sys.stderr, 'Listening on port %i' % port
     sys.stderr.flush()
     server.serve_forever()
 
+application = make_app()
 if __name__ == '__main__':
-    main()
+    run_app(application)
