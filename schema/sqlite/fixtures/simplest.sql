d2856f06db50ac5d9dc3d618d9989e553d11c2d5
Buck Evan <buck.2019@gmail.com>
2017-02-20 13:40:46 -0800

    some schema for sqlite

---
+++ schema/sqlite/fixtures/simplest.sql
@@ -0,0 +1,15 @@
+INSERT INTO "idea"
+    ("title", "description")
+VALUES
+    ("FOO", "BAR"),
+    ("baz", "buz"),
+    ("quux", "qaax")
+;
+
+INSERT INTO "link"
+    ("from-idea", "link-type", "to-idea")
+VALUES
+    (1, 'wut', 2),
+    (2, 'wat', 3),
+    (3, 'huh', 1)
+;
